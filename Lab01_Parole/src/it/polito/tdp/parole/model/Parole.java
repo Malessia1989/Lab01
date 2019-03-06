675171302192ff7d90a042f3ff67c5532e98b392
S157693 <S157693@lepcopi37.cdsdom.polito.it>
2019-03-06 11:19:42 +0100

    lab1

--- Lab01_Parole/src/it/polito/tdp/parole/model/Parole.java
+++ Lab01_Parole/src/it/polito/tdp/parole/model/Parole.java
@@ -1,24 +1,31 @@
 package it.polito.tdp.parole.model;
 
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Parole {
-		
+	List<String> parole= new LinkedList<String>();
 	public Parole() {
-		//TODO
+		
 	}
 	
 	public void addParola(String p) {
-		//TODO
+		if(!parole.contains(p))
+		parole.add(p);
 	}
 	
 	public List<String> getElenco() {
-		//TODO
-		return null;
+		Collections.sort(parole, new ComparatorParole());
+		return parole;
 	}
 	
 	public void reset() {
-		// TODO
+		parole.clear();
+	}
+	
+	public void cancella(String p) {
+		parole.remove(p);
 	}
 
 }
